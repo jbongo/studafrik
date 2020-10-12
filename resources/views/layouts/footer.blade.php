7fdfbeddea9b6fbada6f6c218c38f30242075170
jean philippe <bongojeanphilippe@gmail.com>
2020-10-12 16:25:00 +0200

    creation page d'acceuil et layouts

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,123 @@
+<footer class="gray">
+    <div class="block">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="widget">
+                        <div class="mega-widget">
+                            <div class="logo"><a href="#" title=""><img src="http://placehold.it/93x40" alt="" /></a></div>
+                            <div class="links">
+                                <a href="#" title="">About Us</a>
+                                <a href="#" title="">Terms & Policies</a>
+                                <a href="#" title="">How It Works</a>
+                                <a href="#" title="">Support</a>
+                                <a href="#" title="">Contact Us</a>
+                            </div>
+                            <span>Collins Street West, Victoria 8007, Australia.</span>
+                            <span>+1 246-345-0695</span>
+                            <span>info@jobhunt.com</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="bottom-line style2">
+        <span>© 2018 Jobhunt All rights reserved. Design by Creative Layers</span>
+        <a href="#scrollup" class="scrollup" title=""><i class="la la-arrow-up"></i></a>
+    </div>
+</footer>
+
+</div>
+
+<div class="account-popup-area signin-popup-box">
+<div class="account-popup">
+    <span class="close-popup"><i class="la la-close"></i></span>
+    <h3>User Login</h3>
+    <span>Click To Login With Demo User</span>
+    <div class="select-user">
+        <span>Candidate</span>
+        <span>Employer</span>
+    </div>
+    <form>
+        <div class="cfield">
+            <input type="text" placeholder="Username" />
+            <i class="la la-user"></i>
+        </div>
+        <div class="cfield">
+            <input type="password" placeholder="********" />
+            <i class="la la-key"></i>
+        </div>
+        <p class="remember-label">
+            <input type="checkbox" name="cb" id="cb1"><label for="cb1">Remember me</label>
+        </p>
+        <a href="#" title="">Forgot Password?</a>
+        <button type="submit">Login</button>
+    </form>
+    <div class="extra-login">
+        <span>Or</span>
+        <div class="login-social">
+            <a class="fb-login" href="#" title=""><i class="fa fa-facebook"></i></a>
+            <a class="tw-login" href="#" title=""><i class="fa fa-twitter"></i></a>
+        </div>
+    </div>
+</div>
+</div><!-- LOGIN POPUP -->
+
+<div class="account-popup-area signup-popup-box">
+<div class="account-popup">
+    <span class="close-popup"><i class="la la-close"></i></span>
+    <h3>Sign Up</h3>
+    <div class="select-user">
+        <span>Candidate</span>
+        <span>Employer</span>
+    </div>
+    <form>
+        <div class="cfield">
+            <input type="text" placeholder="Username" />
+            <i class="la la-user"></i>
+        </div>
+        <div class="cfield">
+            <input type="password" placeholder="********" />
+            <i class="la la-key"></i>
+        </div>
+        <div class="cfield">
+            <input type="text" placeholder="Email" />
+            <i class="la la-envelope-o"></i>
+        </div>
+        <div class="dropdown-field">
+            <select data-placeholder="Please Select Specialism" class="chosen">
+                <option>Web Development</option>
+                <option>Web Designing</option>
+                <option>Art & Culture</option>
+                <option>Reading & Writing</option>
+            </select>
+        </div>
+        <div class="cfield">
+            <input type="text" placeholder="Phone Number" />
+            <i class="la la-phone"></i>
+        </div>
+        <button type="submit">Signup</button>
+    </form>
+    <div class="extra-login">
+        <span>Or</span>
+        <div class="login-social">
+            <a class="fb-login" href="#" title=""><i class="fa fa-facebook"></i></a>
+            <a class="tw-login" href="#" title=""><i class="fa fa-twitter"></i></a>
+        </div>
+    </div>
+</div>
+</div><!-- SIGNUP POPUP -->
+
+<script src="{{asset('js/jquery.min.js')}}" type="text/javascript"></script>
+<script src="{{asset('js/modernizr.js')}}" type="text/javascript"></script>
+<script src="{{asset('js/script.js')}}" type="text/javascript"></script>
+<script src="{{asset('js/bootstrap.min.js')}}" type="text/javascript"></script>
+<script src="{{asset('js/wow.min.js')}}" type="text/javascript"></script>
+<script src="{{asset('js/slick.min.js')}}" type="text/javascript"></script>
+<script src="{{asset('js/parallax.js')}}" type="text/javascript"></script>
+<script src="{{asset('js/select-chosen.js')}}" type="text/javascript"></script>
+<script src="{{asset('js/counter.js')}}" type="text/javascript"></script>
+
+</body>
+</html>
