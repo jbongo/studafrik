1ed9206c5d8d73ab83e67ca81575e169269051d2
Jean Philippe <bongojeanphilippe@gmail.com>
2021-04-05 13:43:19 +0200

    welcom

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -63,7 +63,7 @@
                                 </div>
                                 <div class="col-lg-6">
                                     <a href="{{route('nous_contacter')}}" title="">Contact </a>
-                                    <a href="#" title="">Termes du contrat</a>
+                                    {{-- <a href="#" title="">Termes du contrat</a> --}}
                                     <a href="{{route('user.bibliotheque.index')}}" title="">Entreprises </a>
 
                                     {{-- <a href="#" title="">For Employers </a>
